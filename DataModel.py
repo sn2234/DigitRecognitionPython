c8a53e776f2c37e712c982c7ea53f22c834b0619
nobody <nobody@nowhere.com>
2016-02-12 23:39:33 +0200

    Add some files

---
+++ DataModel.py
@@ -0,0 +1,4 @@
+import pandas as pa
+import numpy as np
+
+# load data => {features, label}
--- DigitRecognitionPython.py
+++ DigitRecognitionPython.py
@@ -1,3 +1,18 @@
 import csv
+import numpy as np
 import matplotlib.image as img
+import matplotlib.pyplot as plt
 
+rawData = []
+
+with open("..\\train.csv", "r") as f:
+    reader = csv.reader(f)
+    for i in range(10):
+        rawData.append(next(reader))
+
+tmp = np.array(rawData)
+
+y = tmp[1:, 0].astype(float)
+x = tmp[1:, 1:].astype(float)
+
+plt.imshow((x/255)[0].reshape((28, 28)))
