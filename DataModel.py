36449f3a4e110674ee64899ca963719177b8a039
nobody <nobody@nowhere.com>
2016-02-13 14:51:06 +0200

    CSV import

--- DataModel.py
+++ DataModel.py
@@ -2,3 +2,15 @@ import pandas as pa
 import numpy as np
 
 # load data => {features, label}
+def loadData(fileName):
+    # Load csv into dataframe
+    df = pa.read_csv(fileName)
+    
+    if "label" not in df.columns:
+        df["label"] = None
+
+    pixelColumns = [c for c in df.columns if c != "label"]
+    x = np.array(df[pixelColumns])
+    y = np.array(df["label"])
+
+    return (x, y)
