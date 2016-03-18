Comparing with external implementation and gradient fixes

DigitRecognitionPython.py
@@ -73,7 +73,35 @@ def test2():
     acc_cv = accuracy_score(y_cv, [s.predict(w) for w in x_cv])
     print("Accuracy on CV set: {0}", acc_cv)
 
-test2()
+def compareImplementations():
+    (x, y) = DataModel.loadData("..\\train.csv")
+
+    y = y.astype(int)
+
+    (x_train, x_cv, y_train, y_cv) = DataModel.splitData(x, y)
+
+    x_sub = x_train[:500,:]
+    y_sub = y_train[:500]
+
+    s_my = SimpleNN.SimpleNN([784, 70, 10])
+    s_t = NN_1HL.NN_1HL(reg_lambda = 1, opti_method = 'CG')
+
+    np.random.seed(123)
+
+    thetas = [s_t.rand_init(784,70), s_t.rand_init(70, 10)]
+
+    cost_t = s_t.function(s_t.pack_thetas(thetas[0], thetas[1]), 784, 70, 10, x_sub, y_sub, 1)
+#    grad_t = s_t.function_prime(s_t.pack_thetas(thetas[0], thetas[1]), 784, 70, 10, x_sub, y_sub, 1)
+    print(cost_t);
+
+    cost_my = s_my.computeCost(s_my.combineTheta(thetas), x_sub, y_sub, 1)
+#    grad_my = s_my.computeGrad(s_my.combineTheta(thetas), x_sub, y_sub, 1)
+
+    print(cost_my)
+
+
+compareImplementations()
+
 #bestReg = findBestRegularization(s, x_sub, y_sub)
 
 #clf = svm.SVC(kernel = "rbf", C=0.9)

NN_1HL.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 from scipy import optimize
+from scipy.special import expit
 
 class NN_1HL(object):
     
@@ -8,7 +9,7 @@ class NN_1HL(object):
         self.reg_lambda = reg_lambda
         self.epsilon_init = epsilon_init
         self.hidden_layer_size = hidden_layer_size
-        self.activation_func = self.sigmoid
+        self.activation_func = expit #self.sigmoid
         self.activation_func_prime = self.sigmoid_prime
         self.method = opti_method
         self.maxiter = maxiter

SimpleNN.py
@@ -32,12 +32,13 @@ class SimpleNN:
         for i in range(y.shape[0]):
             yy[i, int(y[i]-1)] = 1
 
+        ones = np.ones((numberOfSamples, 1))
         # Compute activations
-        a1 = np.hstack((np.ones((numberOfSamples, 1)), x))
-        z2 = a1 @ theta[0]
+        a1 = np.hstack((ones, x))
+        z2 = (theta[0].T @ a1.T).T
 
         a2 = np.hstack((np.ones((numberOfSamples, 1)), expit(z2)))
-        z3 = a2 @ theta[1]
+        z3 = (theta[1].T @ a2.T).T
 
         a3 = expit(z3)
 